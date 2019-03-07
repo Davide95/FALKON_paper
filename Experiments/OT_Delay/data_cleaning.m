abadea50b89abc38cda5e4eb05cba9135ca4b7d6
Davide Riva <driva95@protonmail.com>
2019-03-07 08:42:23 +0100

    Manual feature Selection implemented

--- Experiments/OT_Delay/data_cleaning.m
+++ Experiments/OT_Delay/data_cleaning.m
@@ -1,1 +1,27 @@
-t = readtable('dataset/data.csv', 'TreatAsEmpty', 'NA', 'EmptyValue', 0);
+format = '%d%d%d%d%d%s%d%s%C%d%d%d%d%d%d%d%C%C%d%d%d%d%C%d%d%d%d%d%d';
+table = readtable('dataset/1987.csv', ...
+    'TreatAsEmpty', 'NA', ...
+    'EmptyValue', 0, ...
+    'Format', format);
+clear format;
+
+numericalCols = table2array(table(:, {'Distance', 'AirTime', ...
+    'DayOfWeek', 'DayofMonth', 'Month', 'Year'}));
+timeCols = intHmmToMinutes(table2array(table(:, ...
+    {'DepTime', 'ArrTime'})));
+y = table2array(table(:, 'ArrDelay'));
+clear table;
+
+save('dataset/y.mat','y');
+clear y;
+
+X = horzcat(numericalCols, timeCols);
+clear numericalCols timeCols;
+save('dataset/X.mat','X');
+
+function colsOut = intHmmToMinutes(colsIn)
+    hours = floor(colsIn / 100);
+    minutes = colsIn - hours*100;
+    
+    colsOut = hours*60 + minutes;
+end

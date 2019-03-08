9f1465eb10045742e7f7eee2b1f62a378c01d88b
Davide Riva <driva95@protonmail.com>
2019-03-08 11:52:44 +0100

    Data loading, shuffling and splitting implemented

---
+++ Experiments/OT_Delay/test.m
@@ -0,0 +1,23 @@
+%% Load Dataset ----------
+if or(~exist('X' , 'var'), ~exist('y' , 'var'))
+    load('dataset/X.mat');
+    load('dataset/y.mat');
+    disp('Dataset loaded from secondary memory.')
+end
+
+[n, ~] = size(X);
+
+%% Data shuffling ----------
+dataset_shuffling = randperm(n, n);
+X = X(dataset_shuffling, :);
+y = y(dataset_shuffling, :);
+
+%% Data splitting ----------
+ts_size = 100000;
+Xvs = X(1:ts_size, :);
+Yvs = y(1:ts_size);
+Xts = X(ts_size+1:2*ts_size, :);
+Yts = y(ts_size+1:2*ts_size);
+Xtr = X(2*ts_size+1:end, :);
+Ytr = y(2*ts_size+1:end);
+clear X y; % Remove big matrixes from workspace

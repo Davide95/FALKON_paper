91dd68d0e0b2e5f9a0df306bd58321ce1f8bf09e
Davide Riva <driva95@protonmail.com>
2019-03-08 12:07:34 +0100

    Centering of Xtr implemented

--- Experiments/OT_Delay/test.m
+++ Experiments/OT_Delay/test.m
@@ -21,3 +21,11 @@ Yts = y(ts_size+1:2*ts_size);
 Xtr = X(2*ts_size+1:end, :);
 Ytr = y(2*ts_size+1:end);
 clear X y; % Remove big matrixes from workspace
+
+%% Preprocessing
+renorm = @(W, Z) W*(diag(1./(std(Z))).^2);
+recenter = @(W, Z) (renorm(W - ones(size(W,1),1)*mean(Z),Z));
+
+XtrNotCentered = Xtr;
+Xtr = recenter(XtrNotCentered, XtrNotCentered);
+clean XtrNotCentered; % Useless, it's used only to recenter everything
